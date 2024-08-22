5c728fbded2855677926c27763a1ebe42857411b
valegrei <valegreib@gmail.com>
2024-08-21 23:44:48 -0500

    Test UseCases

--- lib/2_application/pages/advice/cubit/advicer_cubit.dart
+++ lib/2_application/pages/advice/cubit/advicer_cubit.dart
@@ -1,5 +1,6 @@
+import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:advicer/1_domain/usecases/advice_usecases.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'advicer_state.dart';
@@ -7,14 +8,14 @@ part 'advicer_state.dart';
 class AdvicerCubit extends Cubit<AdvicerCubitState> {
   AdvicerCubit() : super(AdvicerInitial());
 
+  final AdviceUseCases adviceUseCases = AdviceUseCases();
+
+  //could also use other usecases
+
   void adviceRequest() async {
     emit(AdvicerStateLoading());
-    //execute business logic
-    // for example get and advice
-    debugPrint('fake got advice triggered');
-    await Future.delayed(const Duration(seconds:  3), (){});
-    debugPrint('got advice');
-    emit(const AdvicerStateLoaded(advice: 'fake advice to test bloc'));
+    final AdviceEntity advice = await adviceUseCases.getAdvice();
+    emit(AdvicerStateLoaded(advice: advice.advice));
     //emit(AdvicerStateError(message: 'error message'));
   }
 }
